d469a08519ce06c771d74c777be4b7d8e421bb20
SaaSD3v CI <hearesaas@gmail.com>
2026-09-11 23:52:53 +0000

    droidspaces: materialize Albus 4.9 support and build fixes

--- security/commoncap.c
+++ security/commoncap.c
@@ -31,9 +31,7 @@
 #include <linux/binfmts.h>
 #include <linux/personality.h>
 
-#ifdef CONFIG_ANDROID_PARANOID_NETWORK
 #include <linux/android_aid.h>
-#endif
 
 /*
  * If a non-root user executes a setuid-root binary in
@@ -112,7 +110,6 @@ int cap_capable(const struct cred *cred, struct user_namespace *targ_ns,
 {
 	int ret = __cap_capable(cred, targ_ns, cap, audit);
 
-#ifdef CONFIG_ANDROID_PARANOID_NETWORK
 	if (ret != 0 && cap == CAP_NET_RAW && in_egroup_p(AID_NET_RAW)) {
 		printk("Process %s granted CAP_NET_RAW from Android group net_raw.\n", current->comm);
 		printk("  Please update the .rc file to explictly set 'capabilities NET_RAW'\n");
@@ -125,7 +122,6 @@ int cap_capable(const struct cred *cred, struct user_namespace *targ_ns,
 		printk("  Implicit grants are deprecated and will be removed in the future.\n");
 		return 0;
 	}
-#endif
 	return ret;
 }
 /**
